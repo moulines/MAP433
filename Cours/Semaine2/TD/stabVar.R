2627ae9d429d129bc0588cc38f5d1937ed7e4c4c
MarcLavielle <Marc.Lavielle@gmail.com>
2015-09-01 17:57:41 +0200

    code R pour la feuille PC2 (stabilisation de la variance)

--- Cours/Semaine2/TD/stabVar.R
+++ Cours/Semaine2/TD/stabVar.R
@@ -1,4 +1,9 @@
-xb=0.01
+# on compare, pour différentes valeurs de n, les IC donnés par 2 approximations asymptotiques (2 TCL):
+# - la transformée en arccosinus (de l'exercice)
+# - le TCL "classique" pour \overline{X} en remplaçant la variance de X_i par \overline{X}(1-\overline{X})
+
+
+xb=0.2  # moyenne observée
 alpha=0.05
 
 za=qnorm(1-alpha/2)
@@ -7,5 +12,6 @@ for (n in c(10,100,1000,10000) )
 {
   cat(c(n,'\n'))
 cat(c(0.5*(1+cos(acos(2*xb-1)+za/sqrt(n))), 0.5*(1+cos(acos(2*xb-1)-za/sqrt(n))) ),'\n') 
-cat(c(xb-za*sqrt(xb*(1-xb)/n) , xb+za*sqrt(xb*(1-xb)/n)),'\n')
+cat(c(xb-za*sqrt(xb*(1-xb)/n) , xb+za*sqrt(xb*(1-xb)/n)),'\n\n')
 }
+
