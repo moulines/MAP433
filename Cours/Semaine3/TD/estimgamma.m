4b04b96e5aa1899a85e35ae30d100ab57014b701
moulines <eric.moulines@orange.fr>
2015-09-08 21:40:19 +0200

    MAJ

---
+++ Cours/Semaine3/TD/estimgamma.m
@@ -0,0 +1,28 @@
+%
+%... generation des données
+%
+n= 1000;
+nMC= 500;
+%
+%... estimateur de moments
+%
+halpha= zeros(nMC,1);
+hbeta= zeros(nMC,1);
+hmlea= zeros(nMC,1);
+hmleb= zeros(nMC,1);
+
+for i=1:nMC,
+    X= gamrnd(3,0.5,n,1);
+    barX= mean(X);
+    barlX= mean(log(X));
+    s2= var(X);
+    halpha(i)= barX^2/s2;
+    hbeta(i)= barX/s2;
+%
+%... estimateur du maximum de vraisemblance
+%
+    profile= @(a) -n*a^2*log(a^2)+n*a^2*log(barX)+n*log(gamma(a^2))-n*(a^2-1)*barlX +n*a^2;
+    ha2= fminsearch(profile, halpha(i)^2);
+    hmlea(i)= ha2^2;
+    hmleb(i)= hmlea(i)/barX;
+end
